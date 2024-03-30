8bc48ce4e804cb33f7ebd6e2daef9a7417d9df3f
Soumajyoti <soumyabwn3@gmail.com>
2024-03-30 23:10:04 +0530

    added suspense tag

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, Suspense } from 'react'
 import Navbar from '../components/Navbar'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -147,66 +147,67 @@ const Explore = () => {
     return (
         <>
             <Navbar search={search} setSearch={setSearch} isToken={isToken} setisToken={setisToken} />
+            <Suspense>
+                <p className='text-xl md:text-3xl font-extrabold text-center my-10'>Showing Images: {search}</p>
+                <ToastContainer
+                    position="bottom-left"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
 
-            <p className='text-xl md:text-3xl font-extrabold text-center my-10'>Showing Images: {search}</p>
-            <ToastContainer
-                position="bottom-left"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
 
+                <div className="min-h-screen grid grid-flow-dense grid-cols-1 md:grid-cols-4 gap-2 w-11/12 m-auto">
+                    {
+                        image.map((item, index) => {
+                            return (<div key={item.id} className="flex flex-wrap items-center relative rounded-3xl">
 
-            <div className="min-h-screen grid grid-flow-dense grid-cols-1 md:grid-cols-4 gap-2 w-11/12 m-auto">
-                {
-                    image.map((item, index) => {
-                        return (<div key={item.id} className="flex flex-wrap items-center relative rounded-3xl">
+                                <div className="relative rounded-3xl">
+                                    <Image
+                                        width={400} height={400}
+                                        src={item.urls.regular} alt={item.alt_description}
+                                        className="opacity-100 h-auto max-w-full rounded-3xl cursor-pointer"
+                                    />
 
-                            <div className="relative rounded-3xl">
-                                <Image
-                                    width={400} height={400}
-                                    src={item.urls.regular} alt={item.alt_description}
-                                    className="opacity-100 h-auto max-w-full rounded-3xl cursor-pointer"
-                                />
+                                    <div className="absolute inset-0 flex items-end cursor-pointer ">
+                                        <div className="flex justify-between items-center h-16 w-full px-5 backdrop-blur-sm rounded-3xl">
+                                            <div className="flex">
+                                                <button onClick={() => { handleLike(item.urls.regular, item.id) }} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex ">Like
+                                                    <Image height={20} width={20} src='/like.png' alt="" className=' backdrop-blur-3xl rounded-full ml-4' />
+                                                </button>
 
-                                <div className="absolute inset-0 flex items-end cursor-pointer ">
-                                    <div className="flex justify-between items-center h-16 w-full px-5 backdrop-blur-sm rounded-3xl">
-                                        <div className="flex">
-                                            <button onClick={() => { handleLike(item.urls.regular, item.id) }} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex ">Like
-                                                <Image height={20} width={20} src='/like.png' alt="" className=' backdrop-blur-3xl rounded-full ml-4' />
-                                            </button>
+                                            </div>
 
+                                            <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Save</button>
                                         </div>
-
-                                        <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Save</button>
                                     </div>
                                 </div>
-                            </div>
-                        </div>)
-                    })
-                }
-            </div>
+                            </div>)
+                        })
+                    }
+                </div>
 
-            <div className="flex w-9/12  md:w-1/4 justify-between m-auto mt-10">
-                <button disabled={page == 1} name='dec' onClick={handlePagination} className="flex items-center justify-center px-4 h-10 me-3 text-base font-medium text-gray-300 bg-gray-600 border border-gray-300 rounded-lg hover:bg-gray-700 hover:text-gray-100  disabled:bg-slate-400 disabled:text-gray-300">
-                    <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
-                    </svg>
-                    Previous
-                </button>
-                <button name='inc' onClick={handlePagination} className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Next
-                    <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </button>
-            </div>
+                <div className="flex w-9/12  md:w-1/4 justify-between m-auto mt-10">
+                    <button disabled={page == 1} name='dec' onClick={handlePagination} className="flex items-center justify-center px-4 h-10 me-3 text-base font-medium text-gray-300 bg-gray-600 border border-gray-300 rounded-lg hover:bg-gray-700 hover:text-gray-100  disabled:bg-slate-400 disabled:text-gray-300">
+                        <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
+                        </svg>
+                        Previous
+                    </button>
+                    <button name='inc' onClick={handlePagination} className="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        Next
+                        <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </button>
+                </div>
+            </Suspense>
         </>
     )
 }
