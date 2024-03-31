43a047f2fb0ef91025cb29e0b7584b3b87c342f3
Soumajyoti <soumyabwn3@gmail.com>
2024-03-31 20:06:45 +0530

    gallery  design modified

--- app/mylikes/page.js
+++ app/mylikes/page.js
@@ -10,133 +10,142 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import Link from 'next/link';
 
 const Mylikes = () => {
-    const [isToken, setisToken] = useState(false)
-    const [imageList, setimageList] = useState([]) // To store the liked images
-    const [search, setSearch] = useState('')
+  const [isToken, setisToken] = useState(false)
+  const [imageList, setimageList] = useState([]) // To store the liked images
+  const [search, setSearch] = useState('')
+  const [hovered, setHovered] = useState(false);
 
-    const router = useRouter()
+  const handleClick = () => {
+    setHovered(!hovered);
+    console.log(hovered);
 
-    // To fetch the liked images of that user
-    const fetchData = async (email) => {
-        const data = await fetch('/api/userLikes', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email })
-        })
-        const res = await data.json()
-        console.log(res.userImages[0]);
+  };
 
-        setimageList(res.userImages) // Storing all Liked images of that user into imageList State
+  const router = useRouter()
+
+  // To fetch the liked images of that user
+  const fetchData = async (email) => {
+    const data = await fetch('/api/userLikes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email })
+    })
+    const res = await data.json()
+    console.log(res.userImages[0]);
+
+    setimageList(res.userImages.reverse()) // Storing all Liked images of that user into imageList State. Newest will be first
+  }
+
+  useEffect(() => {
+    const fetchDataAndSetUser = async () => {
+      const token = localStorage.getItem("sessionToken");
+      const userEmail = localStorage.getItem("userEmail");
+      if (token) {
+        setisToken(true); // To pass into Navbar. It will Customise Navbar for loggedin users
+        await fetchData(userEmail);
+      }
+    };
+    fetchDataAndSetUser();
+  }, []);
+
+  // To remove that image from My like if user Clicks on "Remove" button
+  const handleRemove = async (url) => {
+    const userEmail = localStorage.getItem("userEmail");
+    const data = await fetch('/api/removeLike', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: userEmail, url })
+    })
+
+    const res = await data.json()
+    if (res.message === "success") {
+      toast.success('Removed!', {
+        position: "bottom-left",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+      await fetchData(userEmail) // Rerendering My Likes section to show the change
     }
+  }
 
-    useEffect(() => {
-        const fetchDataAndSetUser = async () => {
-            const token = localStorage.getItem("sessionToken");
-            const userEmail = localStorage.getItem("userEmail");
-            if (token) {
-                setisToken(true); // To pass into Navbar. It will Customise Navbar for loggedin users
-                await fetchData(userEmail);
-            }
-        };
-        fetchDataAndSetUser();
-    }, []);
+  /*
+  Getting the DownloadLink from the LikedImage Model. 
+  When saving an image to LikedImage model within the /explore route's handleLike function, 
+  I append {downloadLink: item.links.download + '&force=true'} 
 
-    // To remove that image from My like if user Clicks on "Remove" button
-    const handleRemove = async (url) => {
-        const userEmail = localStorage.getItem("userEmail");
-        const data = await fetch('/api/removeLike', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email: userEmail, url })
-        })
+  So, in every other route, I only need to use item.downloadLink. 
+  However, within /explore, I must include {item.links.download + '&force=true'}  to download an image
+  because I'm directly using the download link from the image, 
+  not from my LikedImage model in the database.
+  */
 
-        const res = await data.json()
-        if (res.message === "success") {
-            toast.success('Removed!', {
-                position: "bottom-left",
-                autoClose: 1000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            })
-            await fetchData(userEmail) // Rerendering My Likes section to show the change
-        }
-    }
+  const handleDownload = (url) => {
+    router.push(url)
+  }
 
-    /*
-    Getting the DownloadLink from the LikedImage Model. 
-    When saving an image to LikedImage model within the /explore route's handleLike function, 
-    I append {downloadLink: item.links.download + '&force=true'} 
+  return (
+    <>
+      <Navbar setSearch={setSearch} isToken={isToken} setisToken={setisToken} />
+      <div className="min-h-screen">
+        <div className="flex justify-center items-center h-28">
+          <p className="text-2xl md:text-3xl font-bold w-fit h-fit bg-slate-300 py-3 px-6 shadow-lg rounded-3xl">My Likes ({imageList.length} images)</p>
+        </div>
+        <ToastContainer
+          position="bottom-left"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
 
-    So, in every other route, I only need to use item.downloadLink. 
-    However, within /explore, I must include {item.links.download + '&force=true'}  to download an image
-    because I'm directly using the download link from the image, 
-    not from my LikedImage model in the database.
-    */
+        {/* Fetching the images from the imageList array */}
+        <div className="ctr  w-11/12 ">
+          {
+            imageList.map((item, index) => {
+              return (<div key={item._id} onClick={handleClick} className="box relative rounded-3xl bg-gray-800">
+                <div className={` ${hovered ? 'imageBox' : ''} imageHoverDesktop transition-opacity duration-300 relative rounded-3xl  max-w-full`}>
+                  <Image
+                    width={400} height={400}
+                    src={item.url} alt={'item'}
+                    className={`imageitem h-auto max-w-full rounded-3xl cursor-pointer `}
+                  />
 
-    const handleDownload = (url) => {
-        router.push(url)
-    }
+                  <div className={`hoverbutton absolute inset-0 cursor-pointer `}>
+                    <div className="flex justify-between items-center h-16 md:w-[60%] w-[48%] mx-auto rounded-3xl mt-10 ">
+                      <div className="flex h-full items-end md:pb-2.5 ">
+                        <button onClick={() => { handleRemove(item.url) }} type="button" className="text-white  md:px-5 md:py-2 text-center  mb-5 md:mb-0 ml-2 flex rounded-lg backdrop-blur-sm  justify-center">
+                          <Image height={47} width={47} src='/delete.png' alt="" className='' />
+                        </button>
+                      </div>
 
-    return (
-        <>
-            <Navbar setSearch={setSearch} isToken={isToken} setisToken={setisToken} />
-            <div className="min-h-screen ">
-                <p className="text-2xl md:text-3xl font-bold text-center my-10">My Favourites ({imageList.length} images)</p>
-                <ToastContainer
-                    position="bottom-left"
-                    autoClose={3000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
-
-                {/* Fetching the images from the imageList array */}
-                <div className="ctr  w-11/12 ">
-                    {
-                        imageList.map((item, index) => {
-                            return (<div key={item._id} className="box  relative rounded-3xl">
-                                <div className="imageBox relative rounded-3xl">
-                                    <Image
-                                        width={400} height={400}
-                                        src={item.url} alt={'item'}
-                                        className="opacity-100 h-auto max-w-full rounded-3xl cursor-pointer"
-                                    />
-
-                                    <div className="absolute inset-0 flex items-end cursor-pointer w-full m-auto">
-                                        <div className="flex justify-between items-end h-full w-full px-3 md:px-6 ">
-                                            <div className="flex h-full items-end md:pb-2.5 ">
-                                                <button onClick={() => { handleRemove(item.url) }} type="button" className="text-white  md:px-5 md:py-2 text-center  mb-5 md:mb-0 ml-2 flex rounded-lg backdrop-blur-md  justify-center">
-                                                    <Image height={35} width={35} src='/delete.png' alt="" className='h-[35px]' />
-                                                </button>
-                                            </div>
-
-                                            <div className="flex h-full items-start md:items-end">
-                                                <button onClick={() => handleDownload(item.downloadLink)} type="button" className=" text-white mt-3 md:px-5 md:py-2.5 text-center mb-2 flex rounded-lg backdrop-blur-md  justify-center">
-                                                    <Image height={35} width={35} src='/download.png' alt="" className='h-[35px]  ' />
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>)
-                        })
-                    }
+                      <div className="flex h-full items-start md:items-end">
+                        <button onClick={() => handleDownload(item.downloadLink)} type="button" className=" text-white mt- md:px-5 md:py-1.5 text-center mb-2 flex rounded-lg backdrop-blur-sm  justify-center">
+                          <Image height={47} width={47} src='/download.png' alt="" className='  ' />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </div>
-        </>
-    )
+              </div>)
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Mylikes
--- app/popular/page.js
+++ app/popular/page.js
@@ -10,152 +10,160 @@ import 'react-toastify/dist/ReactToastify.min.css';
 
 
 const Popular = () => {
-    const [imageList, setimageList] = useState([]) // To store the returned Images from DB
-    const [isLoading, setisLoading] = useState(false)
-    const [email, setEmail] = useState('')
-    const [isToken, setisToken] = useState(false)
-    const [search, setSearch] = useState('')
+  const [imageList, setimageList] = useState([]) // To store the returned Images from DB
+  const [isLoading, setisLoading] = useState(false)
+  const [email, setEmail] = useState('')
+  const [isToken, setisToken] = useState(false)
+  const [search, setSearch] = useState('')
+  const [hovered, setHovered] = useState(false);
 
-    // To navigate User into Login page if they want to like a image without login
-    const router = useRouter()
+  const handleClick = () => {
+    setHovered(!hovered);
+    console.log(hovered);
 
-    // Fetching the most liked images
-    const fetchData = async () => {
-        const data = await fetch('/api/trending')
-        const res = await data.json() // In res.allImages, all trending images are present
-        setimageList(res.allImages) // Saving the most liked images inside imageList state
+  };
+
+  // To navigate User into Login page if they want to like a image without login
+  const router = useRouter()
+
+  // Fetching the most liked images
+  const fetchData = async () => {
+    const data = await fetch('/api/trending')
+    const res = await data.json() // In res.allImages, all trending images are present
+    setimageList(res.allImages.reverse()) // Saving the most liked images inside imageList state
+  }
+
+  useEffect(() => {
+    fetchData()
+
+    const token = localStorage.getItem("sessionToken")
+    const userEmail = localStorage.getItem("userEmail")
+    if (token) {
+      setisToken(true) // To pass into Navbar. It will Customise Navbar for loggedin users
+      setEmail(userEmail)
     }
+  }, [])
 
-    useEffect(() => {
-        fetchData()
+  // User can like a image in this `/popular` route also
+  const handleLike = async (url) => {
+    if (isToken) {
+      const data = await fetch('api/popular', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url, email })
+      })
 
-        const token = localStorage.getItem("sessionToken")
-        const userEmail = localStorage.getItem("userEmail")
-        if (token) {
-            setisToken(true) // To pass into Navbar. It will Customise Navbar for loggedin users
-            setEmail(userEmail)
-        }
-    }, [])
+      const res = await data.json()
+      if (res.message === 'success') {
+        toast.success('Liked!', {
+          position: "bottom-left",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      else {
+        toast.error(res.message, {
+          position: "bottom-left",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
 
-    // User can like a image in this `/popular` route also
-    const handleLike = async (url) => {
-        if (isToken) {
-            const data = await fetch('api/popular', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ url, email })
-            })
-
-            const res = await data.json()
-            if (res.message === 'success') {
-                toast.success('Liked!', {
-                    position: "bottom-left",
-                    autoClose: 1000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-            }
-            else {
-                toast.error(res.message, {
-                    position: "bottom-left",
-                    autoClose: 1000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                });
-            }
-
-        }
-        else {
-            toast.error('Please Login first', {
-                position: "bottom-left",
-                autoClose: 1000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-            setTimeout(() => {
-                router.push("/")
-            }, 2000);
-        }
     }
-
-    /*
-    Getting the DownloadLink from the LikedImage Model. 
-    When saving an image to LikedImage model within the /explore route's handleLike function, 
-    I append {downloadLink: item.links.download + '&force=true'} 
-
-    So, in every other route, I only need to use item.downloadLink. 
-    However, within /explore, I must include {item.links.download + '&force=true'}  to download an image
-    because I'm directly using the download link from the image, 
-    not from my LikedImage model in the database.
-    */
-    const handleDownload = (url) => {
-        router.push(url)
+    else {
+      toast.error('Please Login first', {
+        position: "bottom-left",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      setTimeout(() => {
+        router.push("/")
+      }, 2000);
     }
-    return (
-        <>
-            <Navbar setSearch={setSearch} isToken={isToken} setisToken={setisToken} />
-            <div className="min-h-screen w-screen">
-                <p className="text-2xl font-bold my-10 text-center">Trending Images</p>
-                <ToastContainer
-                    position="bottom-left"
-                    autoClose={3000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
+  }
 
-                {/* Fetching the images from imageList array */}
-                <div className="ctr  w-11/12">
-                    {
-                        imageList.map((item, index) => {
-                            return (<div key={item._id} className="box  relative rounded-3xl">
-                                <div className=" imageBoxrelative rounded-3xl">
-                                    <Image
-                                        width={400} height={400}
-                                        src={item.url} alt={'item'}
-                                        className="opacity-100 h-auto max-w-full rounded-3xl cursor-pointer"
-                                    />
+  /*
+  Getting the DownloadLink from the LikedImage Model. 
+  When saving an image to LikedImage model within the /explore route's handleLike function, 
+  I append {downloadLink: item.links.download + '&force=true'} 
 
-                                    <div className="absolute inset-0 flex items-end cursor-pointer ">
-                                        <div className="flex justify-between items-center h-16 w-full px-5 backdrop-blur-sm rounded-3xl">
-                                            <div className="flex">
-                                                <button onClick={() => { handleLike(item.url) }} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex ">Like
-                                                    <Image height={20} width={20} src='/like.png' alt="" className=' backdrop-blur-3xl rounded-full ml-4' />
-                                                </button>
-                                            </div>
+  So, in every other route, I only need to use item.downloadLink. 
+  However, within /explore, I must include {item.links.download + '&force=true'}  to download an image
+  because I'm directly using the download link from the image, 
+  not from my LikedImage model in the database.
+  */
+  const handleDownload = (url) => {
+    router.push(url)
+  }
+  return (
+    <>
+      <Navbar setSearch={setSearch} isToken={isToken} setisToken={setisToken} />
+      <div className="min-h-screen w-screen">
+        <div className="flex justify-center items-center h-28">
+          <p className="text-lg md:text-3xl font-bold w-fit h-fit bg-slate-300 py-3 px-6 shadow-lg md:shadow-xl hover:shadow-2xl transition-shadow rounded-3xl select-none">Trending Images</p>
+        </div>
+        <ToastContainer
+          position="bottom-left"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
 
-                                            <button onClick={() => handleDownload(item.downloadLink)} type="button" className=" text-white  px-5 py-2.5 text-center me-2 mb-2 flex rounded-lg  justify-center">
-                                                <Image height={35} width={35} src='/download.png' alt="" className='  ' />
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>)
-                        })
-                    }
+        {/* Fetching the images from imageList array */}
+        <div className="ctr  w-11/12">
+          {
+            imageList.map((item, index) => {
+              return (<div key={item._id} onClick={handleClick} className="box relative rounded-3xl bg-gray-800">
+                <div className={` ${hovered ? 'imageBox' : ''} imageHoverDesktop transition-opacity duration-300 relative rounded-3xl  max-w-full`}>
+                  <Image
+                    width={400} height={400}
+                    src={item.url} alt={'item'}
+                    className={`imageitem h-auto max-w-full rounded-3xl cursor-pointer `}
+                  />
+                  <div className={`hoverbutton absolute inset-0 cursor-pointer `}>
+                    <div className="flex justify-between items-center h-16 w-[60%] mx-auto rounded-3xl mt-10">
+                      <div className="flex">
+                        <button onClick={() => { handleLike(item.url) }} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex ">Like
+                          <Image height={20} width={20} src='/like.png' alt="" className=' backdrop-blur-3xl rounded-full ml-4' />
+                        </button>
+                      </div>
+
+                      <button onClick={() => handleDownload(item.downloadLink)} type="button" className=" text-white  py-2.5 text-center  mb-2 flex rounded-lg  justify-center">
+                        <Image height={40} width={40} src='/download.png' alt="" className='  ' />
+                      </button>
+                    </div>
+                  </div>
                 </div>
-            </div>
-        </>
-    )
+              </div>)
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Popular
